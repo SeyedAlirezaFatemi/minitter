404f4c83411153f2777504197fb7ac96e599d1b5
Ali Heydari <ali4heydari@gmail.com>
2018-12-20 19:29:10 +0330

    Add tweet modal to homepage.

--- twitter/views.py
+++ twitter/views.py
@@ -4,6 +4,7 @@ from .models import Tweet
 
 
 def twitter(request):
+    tweets = []
     if request.user.is_authenticated:
         tweets = list(Tweet.objects.all())
     return render(request, 'home.html', {'tweets': tweets})
