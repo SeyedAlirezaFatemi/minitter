b302ad331a895c4330fa3f665251a956afd04ed6
Alireza <alirezafa@rocketmail.com>
2018-12-21 00:23:41 +0330

    tweet.

--- twitter/urls.py
+++ twitter/urls.py
@@ -3,7 +3,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('auth/', include('django.contrib.auth.urls')),
     path('', views.twitter, name='twitter'),
+    path('auth/', include('django.contrib.auth.urls')),
     path('upload_avatar/', views.upload_avatar, name='upload_avatar'),
+    path('new_tweet/', views.new_tweet, name='new_tweet'),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponseForbidden, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
+from django.utils import timezone
 
 from .forms import ImageUploadForm
-from .models import Tweet, Profile
+from .models import Tweet
 
 
 def twitter(request):
@@ -10,9 +12,10 @@ def twitter(request):
     current_user = request.user
     if current_user.is_authenticated:
         tweets = list(Tweet.objects.all())
-    return render(request, 'home.html', {'tweets': tweets})
+    return render(request, 'twitter.html', {'tweets': tweets})
 
 
+@login_required
 def upload_avatar(request):
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
@@ -20,5 +23,15 @@ def upload_avatar(request):
         if form.is_valid():
             current_user.profile.avatar = form.cleaned_data['avatar']
             current_user.save()
-            return HttpResponse('Image upload success.')
+            return HttpResponse('Success')
     return HttpResponseForbidden('Allowed only via POST.')
+
+
+@login_required
+def new_tweet(request):
+    current_user = request.user
+    tweet_title = request.POST.get('tweet_title')
+    tweet_text = request.POST.get('tweet_text')
+    tweet = Tweet(author=current_user, tweet_title=tweet_title, tweet_text=tweet_text, pub_date=timezone.now())
+    tweet.save()
+    return HttpResponse('Success')
