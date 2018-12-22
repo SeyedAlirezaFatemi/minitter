468e44635bf2857929143ec3adc0ee9efc7ab910
Ali Heydari <ali4heydari@gmail.com>
2018-12-22 22:57:54 +0330

    Register loggedinUser in admin.py

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Tweet
+from .models import Profile, Tweet, LoggedInUser
 
 admin.site.register(Tweet)
 admin.site.register(Profile)
+admin.site.register(LoggedInUser)
