7e39e0833407c93ea27005d1da389aafbb8178a4
Ali Heydari <ali4heydari@gmail.com>
2018-12-22 21:57:37 +0330

    Fix avatar bug

--- minitter/settings.py
+++ minitter/settings.py
@@ -119,5 +119,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'avatars'),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGIN_REDIRECT_URL = 'twitter'
 LOGOUT_REDIRECT_URL = 'twitter'
--- twitter/admin.py
+++ twitter/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tweet
+from .models import Tweet, Profile
 
 admin.site.register(Tweet)
+admin.site.register(Profile)
