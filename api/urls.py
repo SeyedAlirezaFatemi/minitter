5f2d1a71844c04976019d6120bb00bef5d9f75cc
Alireza <alirezafa@rocketmail.com>
2018-12-24 22:24:18 +0330

    tokens + tweet.

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-
 from rest_framework.authtoken import views
 
-# Wire up our API using automatic URL routing.
+from .views import new_tweet, update_auth_token
+
 urlpatterns = [
     url('api-token-auth/', views.obtain_auth_token),
+    url('api-update-token/', update_auth_token),
+    url('new-tweet/', new_tweet),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,29 @@
-from rest_framework import viewsets
-
-from .serializers import GroupSerializer, UserSerializer
-from rest_framework import permissions
+from django.utils import timezone
+from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-def login(request):
-    Token.generate_key()
-    print(token.key)
+from twitter.models import Tweet
 
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated,))
+def update_auth_token(request):
+    Token.objects.get(user=request.user).delete()
+    token = Token.objects.create(user=request.user)
+    return Response({
+        'token': token.key,
+    })
+
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+def new_tweet(request):
+    current_user = request.user
+    tweet_title = request.data.get('tweet_title')
+    tweet_text = request.data.get('tweet_text')
+    tweet = Tweet(author=current_user, tweet_title=tweet_title, tweet_text=tweet_text, pub_date=timezone.now())
+    tweet.save()
+    return Response(status=status.HTTP_201_CREATED)
