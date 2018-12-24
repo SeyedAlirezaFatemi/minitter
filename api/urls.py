662fc9f71f7a590ff676026076084ec5ffa11522
Alireza <alirezafa@rocketmail.com>
2018-12-24 23:01:04 +0330

    update token from home.

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,8 @@ from .views import new_tweet, update_auth_token
 
 urlpatterns = [
     url('api-token-auth/', views.obtain_auth_token),
-    url('api-update-token/', update_auth_token),
-    url('new-tweet/', new_tweet),
+    url('api-update-token/', update_auth_token, name='update_token'),
+    url('new-tweet/', new_tweet, name='new_tweet'),
 ]
+
+app_name = 'api'
